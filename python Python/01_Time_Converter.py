6d87759106e5d209de2bce193e2dd3bf1b06622c
samiksha chopade <chopadesamiksha63@gmail.com>
2026-04-09 11:06:15 +0530

    Create 01_Time_Converter.py

---
+++ python Python/01_Time_Converter.py
@@ -0,0 +1,11 @@
+def convert_minutes(minutes):
+    hrs = minutes // 60
+    mins = minutes % 60
+    
+    if hrs > 0:
+        return f"{hrs} hr{'s' if hrs > 1 else ''} {mins} minutes"
+    else:
+        return f"{mins} minutes"
+
+print(convert_minutes(130))  # 2 hrs 10 minutes
+print(convert_minutes(110))  # 1 hr 50 minutes
